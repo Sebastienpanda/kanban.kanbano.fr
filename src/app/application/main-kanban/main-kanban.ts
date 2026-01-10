589b9fddf6ca324250edd836492251cb2a3bb0fe
Sebastienpanda <sebastienprodev@gmail.com>
2026-01-10 17:46:44 +0100

    chore(chore): add supplements paths
    Add supplements alias path to tsconfig
    
    [skip ci]

--- src/app/application/main-kanban/main-kanban.ts
+++ src/app/application/main-kanban/main-kanban.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from "@angular/core";
-import { Aside } from "../shared/ui/aside/aside";
-import { Header } from "../shared/ui/header/header";
-import { Kanban } from "./kanban";
+import { Aside } from "@shared/ui/aside/aside";
+import { Header } from "@shared/ui/header/header";
+import { Kanban } from "../kanban";
 
 @Component({
     selector: "app-main-kanban",
     imports: [Aside, Header, Kanban],
     templateUrl: "./main-kanban.html",
+    styleUrl: "./main-kanban.css",
 })
 export class MainKanban {
     readonly asideOpen = signal(false);
